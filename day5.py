553d90c3cd0014109864045b1d3ccb7e11b186f0
Tennis Kumar C <kumarteneesh@gmail.com>
2022-12-03 20:25:51 +0530

    Day 5 progress

---
+++ day5.py
@@ -0,0 +1,13 @@
+# These are comments
+# Comments are ignored by the computer
+# 100 Days of code Day 5
+print('Hey there I am a good boy\n,hope you are a good girl')
+
+# This is a single line comment 
+
+
+'''
+This is a multiline comment
+'''
+
+print('hey',6,7, sep="+", end=" the end\n")
---
+++ first_pgm.py
@@ -0,0 +1,3 @@
+print('Hello world')
+print(5)
+print('Bye')
