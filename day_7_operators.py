b6673c6b8200b29c46ed5a30fa2f2c033e42a9df
Tennis Kumar C <kumarteneesh@gmail.com>
2022-12-04 19:58:02 +0530

    Day 7 progress, Excercise 1 completed

---
+++ day_7_operators.py
@@ -0,0 +1,13 @@
+# Day 6 - Operators
+
+# Types of operators
+
+# Arithmetic Operators
+
+# +  - addition
+# -  - Subtraction
+# *  - Multiplication
+# ** - Exponential  (eg: 5**3 = 5*5*5)
+# /  - Division
+# // - Floor division returns whole number
+# %  - Modulus (returns remainder)
---
+++ simple_calculator.py
@@ -0,0 +1,15 @@
+a = int(input('Enter the first number: '))
+b = int(input('Enter the Second number: '))
+op = input('Enter the operator: ')
+
+if op == '+':
+  print(a + b)
+elif op == '-':
+  print(a - b)
+elif op == '*':
+  print(a * b)
+elif op == '/':
+  print(a / b)
+
+
+# Can be upgraded to perform advanced calculations using Switch function
