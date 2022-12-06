91134183747adc319dbb5031554c10985656e1d6
Tennis Kumar C <kumarteneesh@gmail.com>
2022-12-06 22:30:04 +0530

    Day 9 Progress

---
+++ day9.py
@@ -0,0 +1,19 @@
+# Day 9 
+
+# Type casting
+
+# Conversion from one data type to another type
+
+
+# int() - converts to integer
+# float() - converts to float
+# str() - converts to string
+
+a = '3 '
+b = 4
+a2 = 'harry'
+b2 = '100 Days of code'
+
+print(a + str(b))
+
+print(a + b2)
