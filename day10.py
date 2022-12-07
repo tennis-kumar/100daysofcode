66b6537c0a129a82c1d3b3560d60b9af9b48d464
Tennis Kumar C <kumarteneesh@gmail.com>
2022-12-07 22:01:39 +0530

    Day 10 progress

---
+++ day10.py
@@ -0,0 +1,21 @@
+# Day 10 
+
+# Taking inputs from user
+
+a = input()
+
+print('value of a is ',a)
+
+# input() will take input as a string
+
+# if you want to take input as integer then use int(input())
+
+# if you want to take input as float then use float(input())
+
+# use typecasting to convert to desired form
+
+# typecasting is done by using int(), float(), str() etc
+
+# typecasting is done to convert one data type to another
+
+b = int(input('this will take input as an integer: '))
